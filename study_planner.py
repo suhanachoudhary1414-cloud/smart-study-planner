7300b37b09dec358db488b979c6152ded4bd4380
Suhana Choudhary <suhanachoudhary1414@gmail.com>
2026-06-26 22:20:19 +0530

    Added task deletion feature

--- study_planner.py
+++ study_planner.py
@@ -7,7 +7,8 @@ while True:
 
     print("1. Add Task")
     print("2. View Tasks")
-    print("3. Exit")
+    print("3. Delete Task")
+    print("4. Exit")
 
     choice = input("\nChoose an option: ")
 
@@ -21,12 +22,28 @@ while True:
 
         if len(tasks) == 0:
             print("No tasks yet.")
-
         else:
             for i, task in enumerate(tasks, start=1):
                 print(f"{i}. {task}")
 
     elif choice == "3":
+        if len(tasks) == 0:
+            print("\nNo tasks to delete.")
+        else:
+            print("\nYour Tasks:")
+
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
+
+            delete = int(input("\nEnter task number to delete: "))
+
+            if 1 <= delete <= len(tasks):
+                removed = tasks.pop(delete - 1)
+                print(f"\n✅ '{removed}' deleted successfully!")
+            else:
+                print("\n❌ Invalid task number.")
+
+    elif choice == "4":
         print("\nGoodbye! 👋")
         break
 
