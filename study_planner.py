0005535c26aa60cbfe129f8b90b5112981739733
Suhana Choudhary <suhanachoudhary1414@gmail.com>
2026-06-26 23:00:13 +0530

    Added task completion feature

--- study_planner.py
+++ study_planner.py
@@ -8,13 +8,20 @@ while True:
     print("1. Add Task")
     print("2. View Tasks")
     print("3. Delete Task")
-    print("4. Exit")
+    print("4. Mark Task as Completed")
+    print("5. Exit")
 
     choice = input("\nChoose an option: ")
 
     if choice == "1":
         task = input("Enter your task: ")
-        tasks.append(task)
+
+        new_task = {
+            "name": task,
+            "completed": False
+        }
+
+        tasks.append(new_task)
         print("\n✅ Task added successfully!")
 
     elif choice == "2":
@@ -24,26 +31,52 @@ while True:
             print("No tasks yet.")
         else:
             for i, task in enumerate(tasks, start=1):
-                print(f"{i}. {task}")
+                status = "✓" if task["completed"] else " "
+                print(f"{i}. [{status}] {task['name']}")
 
     elif choice == "3":
+
         if len(tasks) == 0:
             print("\nNo tasks to delete.")
+
         else:
             print("\nYour Tasks:")
 
             for i, task in enumerate(tasks, start=1):
-                print(f"{i}. {task}")
+                status = "✓" if task["completed"] else " "
+                print(f"{i}. [{status}] {task['name']}")
 
             delete = int(input("\nEnter task number to delete: "))
 
             if 1 <= delete <= len(tasks):
                 removed = tasks.pop(delete - 1)
-                print(f"\n✅ '{removed}' deleted successfully!")
+                print(f"\n✅ '{removed['name']}' deleted successfully!")
+
             else:
                 print("\n❌ Invalid task number.")
 
     elif choice == "4":
+
+        if len(tasks) == 0:
+            print("\nNo tasks available.")
+
+        else:
+            print("\nYour Tasks:")
+
+            for i, task in enumerate(tasks, start=1):
+                status = "✓" if task["completed"] else " "
+                print(f"{i}. [{status}] {task['name']}")
+
+            complete = int(input("\nEnter task number to mark as completed: "))
+
+            if 1 <= complete <= len(tasks):
+                tasks[complete - 1]["completed"] = True
+                print("\n✅ Task marked as completed!")
+
+            else:
+                print("\n❌ Invalid task number.")
+
+    elif choice == "5":
         print("\nGoodbye! 👋")
         break
 
